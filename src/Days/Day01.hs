04ffe0ca5569fe1aee3f869a0bbf5cc7a57c3985
Jack Garner <jacksonhgarner@outlook.com>
2020-11-30 23:12:23 -0700

    Day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -14,6 +14,8 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Functor.Foldable
+import Control.Applicative
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +23,28 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy1' decimal endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = fromJust . para (findIt 2020)
+
+-- I think there's something interesting here about the structure but I'm not sure...
+findIt :: Int -> ListF Int ([Int], Maybe Int) -> Maybe Int
+findIt i (Cons x (xs, result)) = result <|> fmap (* x) (find (\y -> y + x == i) xs)
+findIt _ Nil = Nothing
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = fromJust . para findItB
+  
+findItB :: ListF Int ([Int], Maybe Int) -> Maybe Int
+findItB (Cons x (xs, result)) = result <|> fmap (* x) (para (findIt (2020 - x)) xs)
+findItB Nil = Nothing
