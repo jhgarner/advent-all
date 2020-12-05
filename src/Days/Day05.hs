5f1d00312d5f85634c04306328482c5c28a72291
Jack Garner <jacksonhgarner@outlook.com>
2020-12-04 22:36:42 -0700

    Add day 5

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,7 +1,11 @@
 module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.List hiding (take)
+import Prelude hiding (take)
+import Util.Util
+import Data.Monoid (Product(..), Sum(..))
+import Data.Semigroup
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -10,6 +14,7 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Data.Text (Text, unpack, pack)
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
@@ -21,19 +26,33 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = flip sepBy1' endOfLine $ do
+  row <- unpack <$> take 7
+  seat <- unpack <$> take 3
+  pure $ ( map (\a -> if a == 'F' then 0 else 1) row
+         , map (\a -> if a == 'L' then 0 else 1) seat
+         )
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [([Int], [Int])]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Maybe (Int, Int)
 
 ------------ PART A ------------
+toInt :: [Int] -> Int
+toInt [] = 0
+toInt (l:ls) = l * 2^(length ls) + toInt ls
+
+toSeatScore :: ([Int], [Int]) -> Int
+toSeatScore (a, b) = toInt a * 8 + toInt b
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = maximum . fmap toSeatScore
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input =
+  let ls = sort $ fmap toSeatScore input
+   in find (\(a, b) -> b - a == 2) $ zip ls (tail ls)
