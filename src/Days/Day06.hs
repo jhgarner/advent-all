1c2cc01045d04c3e152593cf1f235a0740687c4d
Jack Garner <jacksonhgarner@outlook.com>
2020-12-06 00:10:12 -0700

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,15 +1,16 @@
 module Days.Day06 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.List hiding (intersect)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Set (Set)
+import Data.Set (Set, fromList, intersection)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Data.Monoid
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
@@ -21,19 +22,27 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = flip sepBy1' endOfLine $ do
+  sepBy (many1' letter) endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[[Char]]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+groupToSet :: [[Char]] -> Set Char
+groupToSet = foldMap fromList
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = getSum . foldMap (Sum . length . groupToSet)
 
 ------------ PART B ------------
+groupToSetI :: [[Char]] -> Set Char
+groupToSetI [] = mempty
+groupToSetI (l:ls) = foldl' (\acc a -> acc `intersection` fromList a) (fromList l) ls 
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = getSum . foldMap (Sum . length . groupToSetI)
