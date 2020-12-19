e53b5413a4c6e981e3a695dfb0bbc382fe124a1f
Jack Garner <jgarner@mines.edu>
2020-12-19 10:35:12 -0700

    Day 19

--- src/Days/Day19.hs
+++ src/Days/Day19.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Days.Day19 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -14,26 +17,72 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Applicative
+import Debug.Trace
+import Data.Functor.Foldable
+import Data.Functor.Foldable.TH
+import Data.Foldable
 {- ORMOLU_ENABLE -}
+
+data Rule = Lit Char | Pipe [[Rule]]
+  deriving (Show, Eq)
+
+makeBaseFunctor ''Rule
+  
+instance Show (RuleF Int) where
+  show = const "No"
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (,) <$> (rules <* endOfLine) <*> input
+  where
+    rules = fmap Map.fromList $ flip sepBy1' endOfLine $ do
+      ident <- decimal
+      char ':'
+      lhs <- lit <|> pipe
+      pure $ (ident, lhs)
+    lit = LitF <$> (string " \"" *> anyChar <* char '"')
+    ref = char ' ' *> decimal
+    chain = many1' ref
+    pipe = PipeF <$> sepBy1' chain (string " |")
+    input = endOfLine >> sepBy1' (many1' letter) endOfLine
+    
 
 ------------ TYPES ------------
-type Input = Void
+toFixed :: Map Int (RuleF Int) -> Int -> RuleF Int
+toFixed m i = m Map.! i
 
-type OutputA = Void
+parseInput :: RuleF (String -> Maybe String) -> String -> Maybe String
+parseInput (LitF _) [] = Nothing
+parseInput (LitF l) s = if head s == l then Just $ tail s else Nothing
+parseInput (PipeF ls) s = asum $ fmap (foldl' (>>=) $ Just s) ls
 
-type OutputB = Void
+parseInputB :: RuleF ([String] -> [Maybe String]) -> [String] -> [Maybe String]
+parseInputB _ [] = []
+parseInputB (LitF l) ls = filter (not . null) $ fmap (\case
+                                                        c:cs -> if c == l then Just cs else Nothing
+                                                        [] -> Nothing) ls
+parseInputB (PipeF ls) ss = filter (not . null) $ concatMap (($ ss) . foldl1 (\acc a -> a . fmap fromJust . acc)) ls
+
+type Input = (Map Int (RuleF Int), [String])
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (m, inputs) = length $ filter (== Just "") $ fmap (cata parseInput rule) inputs
+  where
+    rule :: Rule
+    rule = ana (toFixed m) 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (m, inputs) = length $ filter (elem $ Just "") $ fmap (cata parseInputB rule . pure) inputs
+  where
+    rule :: Rule
+    rule = ana (toFixed m) 0
